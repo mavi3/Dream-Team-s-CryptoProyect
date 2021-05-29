47f36d32fe34e34cfc9faa5b527547fdc8c34ca0
Marko <56947440+mavi3@users.noreply.github.com>
2021-05-29 19:43:42 -0400

    Nombre BD y pass

--- db_config.php
+++ db_config.php
@@ -1,6 +1,6 @@
 <?php
 /* Detalles de la conexión */
-$conn_string = "host=localhost port=5432 dbname=<nombre_db> user=postgres password=<contraseña>";
+$conn_string = "host=localhost port=5432 dbname=Tarea-1 user=postgres password=DreamPassword69";
 // Recuerde reemplazar "<contraseña>" por su contraseña y "<nombre_db>" por el nombre de su BD. No se incluyen los "<>".
 // Establecemos una conexión con el servidor postgresSQL
 $dbconn = pg_connect($conn_string);
