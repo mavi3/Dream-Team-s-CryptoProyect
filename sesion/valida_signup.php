4997e32bdec0a6982668918786e90a66108dd988
Juan Mira <juan.mirag@usm.cl>
2021-05-30 16:08:23 -0400

    Corrección embarradita de juan

--- sesion/valida_signup.php
+++ sesion/valida_signup.php
@@ -9,36 +9,42 @@ $correo = isset($_POST['email']) ? $_POST['email'] : '';
 $password = isset($_POST['pwd']) ? $_POST['pwd'] : '';
 $password2 = isset($_POST['pwd2']) ? $_POST['pwd2'] : '';
 $pais = isset($_POST['country']) ? $_POST['country'] : '';
-echo $nombre;
 
 if($password == $password2){
-    
-    $opciones = array('cost'=>12);
-    $password = password_hash($password,PASSWORD_BCRYPT,$opciones);
-    $consulta = "INSERT 
-        INTO usuario(nombre,apellido,correo,contraseña,pais,fecha_registro,admin)
-        VALUES(
-            $1,
-            $2,
-            $3,
-            $4,
-            $5,
-            (SELECT NOW()),
-            false)";
+  
+    $comprobar_mail = pg_query_params($dbconn,"SELECT correo FROM usuario WHERE correo =$1",array($correo));
+    $a= pg_fetch_row($comprobar_mail);
+    if(!$comprobar_mail){
+        $opciones = array('cost'=>12);
+        $password = password_hash($password,PASSWORD_BCRYPT,$opciones);
+        $consulta = "INSERT 
+            INTO usuario(nombre,apellido,correo,contraseña,pais,fecha_registro,admin)
+            VALUES(
+                $1,
+                $2,
+                $3,
+                $4,
+                $5,
+                (SELECT NOW()),
+                false)";
 
-    $envio = pg_query_params($dbconn, $consulta, array($nombre,$apellido,$correo,$password,$pais));
-    if($envio){
-        echo "izi <br /><br />";
-        header("refresh:5;url=log-in.html");
-        echo 'Tu cuenta ha sido creada, serás redireccionado en 5 segundos para que puedas logearte. Si no quieres esperar, haz click <a href="../index.html">aquí</a>.';
-    }else{
-        echo "nope <br /><br />";
-        header("refresh:5;url=sign-up.html");
-        echo "BAD ENDING Inténtalo nuevamente.";
+        $envio = pg_query_params($dbconn, $consulta, array($nombre,$apellido,$correo,$password,$pais));
+        if($envio){
+            header("refresh:5;url=log-in.html");
+            echo 'Tu cuenta ha sido creada, serás redireccionado en 5 segundos para que puedas logearte. Si no quieres esperar, haz click <a href="../index.html">aquí</a>.';
+        }else{
+            header("refresh:5;url=sign-up.html");
+            echo "BAD ENDING Inténtalo nuevamente.";
+        }
+
+}else{
+    header("refresh:5;url=sign-up.html");
+    echo "El Coreo ingresado Ya existe, por favor ingresa uno distinto.";
     }
 
-    
- 
+}else{
+    header("refresh:5;url=sign-up.html");
+    echo "BAD Try Las Contraseñas no son iguales :/. Por Favor Reintenta";
 }
 pg_close($dbconn)
 
