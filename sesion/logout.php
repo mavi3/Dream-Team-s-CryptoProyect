88f15e10722ed5ecd03cffba801e29dbb0beaae4
Lord-Ureiva <Javier.peraltae@usm.cl>
2021-05-30 02:40:09 -0400

    Valida login + valida sesion

--- sesion/logout.php
+++ sesion/logout.php
@@ -1,3 +1,9 @@
 <?php
 /* Este archivo debe manejar la lógica de cerrar una sesión */
+session_start();
+//Matamos la sesion
+session_destroy();
+header( "refresh:2;url=../index.html" );
+echo 'Haz cerrado sesión con éxito.<br /><br />';
+echo 'Serás redireccionado en 2 segundos.';
 ?>
