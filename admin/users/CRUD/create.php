0cad91f8fa40117f9acb856f37e5ae7501bdb48b
Juan Mira <juan.mirag@usm.cl>
2021-05-30 04:10:06 -0400

    Delete implementación

--- admin/users/CRUD/create.php
+++ admin/users/CRUD/create.php
@@ -1,3 +1,28 @@
 <?php
 /* Este archivo debe manejar la lógica para crear un usuario como admin */
+include $_SERVER['DOCUMENT_ROOT'].'/db_config.php';
+
+$nombre = isset($_POST['name']) ? $_POST['name'] : '';
+$apellido = isset($_POST['surname']) ? $_POST['surname'] : '';
+$correo = isset($_POST['email']) ? $_POST['email'] : '';
+$pwd = isset($_POST['pwd']) ? $_POST['pwd'] : '';
+$pais = isset($_POST['country']) ? $_POST['country'] : '';
+
+$opciones = array('cost'=>12);
+$pwd = password_hash($pwd,PASSWORD_BCRYPT,$opciones);
+
+$consulta = "INSERT 
+    INTO usuario(nombre,apellido,correo,contraseña,pais,fecha_registro,admin)
+    VALUES(
+        $1,
+        $2,
+        $3,
+        $4,
+        $5,
+        (SELECT NOW()),
+        false)";
+
+$envio = pg_query_params($dbconn, $consulta, array($nombre,$apellido,$correo,$pwd,$pais));
+pg_close($dbconn);
+header('Location: ../all.html');
 ?>
--- admin/users/CRUD/delete.php
+++ admin/users/CRUD/delete.php
@@ -1,3 +1,12 @@
 <?php
-/* Este archivo debe manejar la lógica de borrar un usuario (y los registros relacionados) como admin */
+include $_SERVER['DOCUMENT_ROOT'].'/db_config.php';
+
+echo $id = isset($_GET['id']) ? $_GET['id'] : '';
+$consulta = "DELETE
+FROM usuario
+WHERE id = $1";
+
+$enviar = pg_query_params($dbconn,$consulta ,array($id));
+pg_close($dbconn);
+header('Location: ../all.html');
 ?>
