822f6acac77014d4944f3fca6dfc2e23446cb8d4
Lord-Ureiva <Javier.peraltae@usm.cl>
2021-05-30 03:21:17 -0400

    Cambios varios y profile
    Entre los cambios:
    -solucionado bug en all.php
    -ahora se obtienen todos los datos del usuario a partir de su id
    
    Se terminó la página profile

--- user/profile.php
+++ user/profile.php
@@ -1,3 +1,11 @@
 <?php
 /* Este archivo debe manejar la lógica para obtener la información del perfil */
+$id = $_SESSION['id'];
+
+$consulta = pg_query("SELECT id, usuario.nombre, apellido, correo, pais.nombre, DATE(fecha_registro) FROM usuario INNER JOIN pais ON usuario.pais = pais.cod_pais WHERE id=".$id);
+$fila = pg_fetch_row($consulta);
+echo "<p>Nombre Completo: ".$fila[1]. " " .$fila[2]." </p>";
+echo "<p>Correo: ". $fila[3]." </p>";
+echo "<p>País: ". $fila[4]." </p>";
+echo "<p>Fecha de Ingreso: ". $fila[5]." </p>";     
 ?>
