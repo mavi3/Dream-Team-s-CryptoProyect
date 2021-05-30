3ec748bede2292ff8cf1060093976ac5d8cf5b66
Marko <56947440+mavi3@users.noreply.github.com>
2021-05-30 16:00:13 -0400

    ARREGLADO LO QUE SE ECHO EL JUAN

--- sesion/valida_signup.php
+++ sesion/valida_signup.php
@@ -13,32 +13,39 @@ echo $nombre;
 
 if($password == $password2){
     
-    $opciones = array('cost'=>12);
-    $password = password_hash($password,PASSWORD_BCRYPT,$opciones);
-    $consulta = "INSERT 
-        INTO usuario(nombre,apellido,correo,contraseña,pais,fecha_registro,admin)
-        VALUES(
-            $1,
-            $2,
-            $3,
-            $4,
-            $5,
-            (SELECT NOW()),
-            false)";
+    $comprobar_mail = pg_query_params($dbconn,"SELECT correo FROM usuario WHERE correo =$1",array($correo));
+    if(!$comprobar_mail){
+        $opciones = array('cost'=>12);
+        $password = password_hash($password,PASSWORD_BCRYPT,$opciones);
+        $consulta = "INSERT 
+            INTO usuario(nombre,apellido,correo,contraseña,pais,fecha_registro,admin)
+            VALUES(
+                $1,
+                $2,
+                $3,
+                $4,
+                $5,
+                (SELECT NOW()),
+                false)";
 
-    $envio = pg_query_params($dbconn, $consulta, array($nombre,$apellido,$correo,$password,$pais));
-    if($envio){
-        echo "izi <br /><br />";
-        header("refresh:5;url=log-in.html");
-        echo 'Tu cuenta ha sido creada, serás redireccionado en 5 segundos para que puedas logearte. Si no quieres esperar, haz click <a href="../index.html">aquí</a>.';
+        $envio = pg_query_params($dbconn, $consulta, array($nombre,$apellido,$correo,$password,$pais));
+        if($envio){
+            header("refresh:5;url=log-in.html");
+            echo 'Tu cuenta ha sido creada, serás redireccionado en 5 segundos para que puedas logearte. Si no quieres esperar, haz click <a href="../index.html">aquí</a>.';
+        }else{
+            header("refresh:5;url=sign-up.html");
+            echo "BAD ENDING Inténtalo nuevamente.";
+        }
     }else{
-        echo "nope <br /><br />";
         header("refresh:5;url=sign-up.html");
-        echo "BAD ENDING Inténtalo nuevamente.";
-    }
+        echo "El Correo ingresado ya está registrado, por favot ingrese otro.";
 
+    }
     
  
+}else{
+    header("refresh:5;url=sign-up.html");
+    echo "BAD Try Las Contraseñas no son iguales :/. Por Favor Reintenta";
 }
 pg_close($dbconn)
 
