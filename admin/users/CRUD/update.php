1b4093260202aa93cdd2fb57311079e65ee8f8b7
Juan Mira <juan.mirag@usm.cl>
2021-05-30 03:43:09 -0400

    hash pwd update.php

--- admin/users/CRUD/update.php
+++ admin/users/CRUD/update.php
@@ -8,6 +8,9 @@ $correo = isset($_POST['email']) ? $_POST['email'] : '';
 $pwd = isset($_POST['pwd']) ? $_POST['pwd'] : '';
 $pais = isset($_POST['country']) ? $_POST['country'] : '';
 
+$opciones = array('cost'=>12);
+$pwd = password_hash($pwd,PASSWORD_BCRYPT,$opciones);
+
 $consulta = 
 "UPDATE usuario
 SET nombre = $1, apellido = $2, correo = $3, contraseña = $4, pais = $5
@@ -15,5 +18,6 @@ WHERE id =$6";
 $enviar = pg_query_params($dbconn,$consulta, array($nombre, $apellido, $correo, $pwd, $pais, $id));
 
 pg_close($dbconn);
+header('Location: ../all.html');
 
 ?>
