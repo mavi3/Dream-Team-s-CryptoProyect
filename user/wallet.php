e5ed0ed692c1f858a9a7d536267339098ba68eec
Marko <56947440+mavi3@users.noreply.github.com>
2021-05-30 20:57:19 -0400

    Consulta de wallet lista

--- user/wallet.php
+++ user/wallet.php
@@ -2,10 +2,13 @@
 include $_SERVER['DOCUMENT_ROOT'].'/db_config.php';
 /* Este archivo debe manejar la lógica para obtener la información de la billetera */
 $id = $_SESSION['id'];
-$sql = "SELECT moneda.sigla, moneda.nombre, usuario_tiene_moneda.balance
-        FROM usuario_tiene_moneda INNER JOIN moneda
-        ON usuario_tiene_moneda.id_moneda = moneda.id
-        WHERE id_usuario = $1";
+$sql = "SELECT DISTINCT ON (a.sigla) a.sigla, a.nombre, a.balance, precio_moneda.valor
+        FROM (SELECT usuario_tiene_moneda.id_usuario, moneda.id, moneda.sigla, moneda.nombre, usuario_tiene_moneda.balance
+            FROM usuario_tiene_moneda INNER JOIN moneda
+            ON usuario_tiene_moneda.id_moneda = moneda.id) as a INNER JOIN precio_moneda
+        ON a.id = precio_moneda.id_moneda
+        WHERE a.id_usuario = $1
+        ORDER BY a.sigla, precio_moneda.fecha DESC NULLS LAST";
 $consulta = pg_query_params($dbconn, $sql,array($id));
 /* Comprobar la consulta */
 
@@ -18,6 +21,8 @@ if(!$consulta){
         echo '<td>'. $fila[0]. '</td>';
         echo '<td>'. $fila[1]. '</td>';
         echo '<td>'. $fila[2]. '</td>';
+        echo '<td>'. $fila[3]. ' USD </td>';
+        echo '<td>'. $fila[3] * $fila[2]. ' USD </td>';
         echo '</tr>';
     }
 }
