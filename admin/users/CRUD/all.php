b55d6b2c3726bdafafe0819b82b243860448cd98
Marko <56947440+mavi3@users.noreply.github.com>
2021-06-09 23:26:38 -0400

    Boostrap animacion confirmacion Borrar

--- admin/users/CRUD/all.php
+++ admin/users/CRUD/all.php
@@ -30,8 +30,41 @@ if(!$consulta){
                             class="fas fa-search"></i></a>
                     <a href="/admin/users/update.html?id='.$fila[0].'" class="btn btn-warning">Editar <i
                             class="fas fa-edit"></i></a>
-                    <a href="/admin/users/CRUD/delete.php?id='.$fila[0].'" class="btn btn-danger">Borrar <i
-                            class="fas fa-trash-alt"></i></a>
+
+                    
+                    
+                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#myModal">
+                            Borrar <i
+                            class="fas fa-trash-alt"></i>
+                          </button>
+                          
+                          <!-- The Modal -->
+                          <div class="modal fade" id="myModal">
+                            <div class="modal-dialog">
+                              <div class="modal-content">
+                          
+                                <!-- Modal Header -->
+                                <div class="modal-header">
+                                  <h4 class="modal-title">Borrar Usuario</h4>
+                                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                </div>
+                          
+                                <!-- Modal body -->
+                                <div class="modal-body">
+                                  ¿Estás seguro de que deseas borrar al usuario?
+                                </div>
+                          
+                                <!-- Modal footer -->
+                                <div class="btn right" >
+                                    <a href="/admin/users/CRUD/delete.php?id='.$fila[0].'" class="btn btn-danger">Borrar <i
+                                    class="fas fa-trash-alt"></i></a>
+                                  <button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>
+                                </div>
+                          
+                              </div>
+                            </div>
+                          </div>
+
                     </td>';
 
         echo '</tr>';
--- admin/users/CRUD/read.php
+++ admin/users/CRUD/read.php
@@ -17,7 +17,40 @@ echo "<p>Fecha de Ingreso: ". $fila[5]." </p>";
 echo '<div class="d-flex justify-content-end">
          <a href="/admin/users/all.html" class="btn btn-secondary">Volver</a>
          <a href="/admin/users/update.html?id='. $fila[0].'" class="btn btn-warning mx-3">Editar <i class="fas fa-edit"></i></a>
-         <a href="/admin/users/CRUD/delete.php?id='. $fila[0].'" class="btn btn-danger">Borrar <i class="fas fa-trash-alt"></i></a></td>
+         
+
+         <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#myModal">
+                            Borrar <i
+                            class="fas fa-trash-alt"></i>
+                          </button>
+                          
+                          <!-- The Modal -->
+                          <div class="modal fade" id="myModal">
+                            <div class="modal-dialog">
+                              <div class="modal-content">
+                          
+                                <!-- Modal Header -->
+                                <div class="modal-header">
+                                  <h4 class="modal-title">Borrar Usuario</h4>
+                                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                </div>
+                          
+                                <!-- Modal body -->
+                                <div class="modal-body">
+                                  ¿Estás seguro de que deseas borrar al usuario?
+                                </div>
+                          
+                                <!-- Modal footer -->
+                                <div class="btn right" >
+                                    <a href="/admin/users/CRUD/delete.php?id='.$fila[0].'" class="btn btn-danger">Borrar <i
+                                    class="fas fa-trash-alt"></i></a>
+                                  <button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>
+                                </div>
+                          
+                              </div>
+                            </div>
+                          </div>
+         
      </div>';
 
 pg_close($dbconn);
